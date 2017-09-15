63dba533471ebd070cdc6844d21fa7cbd043b5f8
snafx <k.ozegowski@gmail.com>
2017-09-15 16:41:54 +0200

    first commit

---
+++ src/main/java/com/snafx/TripDiary/TripDiaryApplication.java
@@ -0,0 +1,12 @@
+package com.snafx.TripDiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripDiaryApplication.class, args);
+	}
+}
