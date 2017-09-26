89bc8eda5113b385dacb774231194bfdbc83e90b
snafx <k.ozegowski@gmail.com>
2017-09-26 14:06:57 +0200

    minor login fix and added project screenshots

--- src/main/java/com/snafx/TripDiary/configs/WebSecurityConfig.java
+++ src/main/java/com/snafx/TripDiary/configs/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -35,6 +36,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .permitAll()
                     .and()
                 .logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                     .permitAll();
     }
 
